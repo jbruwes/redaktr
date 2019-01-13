5f3dc78c4871f2d16893f46ce2230eed4717b4a6
Jerry Bruwes <jbruwes@icloud.com>
2019-01-13 16:36:16 +0000

    SingInView alpha stage

--- sources/views/signin.js
+++ sources/views/signin.js
@@ -3,18 +3,22 @@ export default class SignInView extends JetView {
     config() {
         return {
             css: "signInView",
-            rows: [
-                { template: "Start page" },
-                {},
-                {
-                    cols: [
-                        {},
-                        { template: "Start page" },
-                        {}
-                    ]
-                },
-                {}
-            ]
+            cols: [{ gravity: 0.38 }, {
+                css: "signInViewRight",
+                rows: [{ gravity: 2 }, {
+                    css: "signInViewField",
+                    padding: 30,
+                    cols: [{
+                        id: "a1",
+                        rows: [{
+                            responsive: "a1",
+                            cols: [{ minWidth: 200, view: "button", label: "Sign In with Facebook", type: "iconButton", icon: "mdi mdi-facebook" },
+                                { minWidth: 200, view: "button", label: "Sign In with Google", type: "iconButton", icon: "mdi mdi-google" }
+                            ]
+                        }]
+                    }, { gravity: 0.38 }]
+                }, {}]
+            }]
         };
     }
 }
