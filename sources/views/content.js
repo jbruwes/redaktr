f75698e743b3606d6d0752978f67d3715ccaef79
Jerry Bruwes <jbruwes@icloud.com>
2019-01-25 12:35:07 +0000

    chpock

--- sources/views/content.js
+++ sources/views/content.js
@@ -2,27 +2,28 @@ import { JetView } from "webix-jet";
 export default class ContentView extends JetView {
 	config() {
 		var lastXHRGet = null;
-		var lastXHRPost = null;
+		var lastXHRTreePost = null;
 		webix.attachEvent("onBeforeAjax", function(mode, url, params, xhr) {
-			if (mode === 'POST') {
-				lastXHRPost = xhr;
+			console.log(url);
+			if (mode === 'POST' && url === 'https://api.redaktr.com/tree') {
+				lastXHRTreePost = xhr;
 			}
-			else {
+			if (mode === 'GET') {
 				lastXHRGet = xhr;
 			}
 		});
 		var onChangeFnc = function(id) {
 			webix.delay(() => {
-				lastXHRPost.abort();
-				webix.ajax().post("https://api.redaktr.com/index", webix.ajax().stringify($$("tree").data.serialize()), function(text, xml, xhr) {
+				if (lastXHRTreePost) { lastXHRTreePost.abort(); }
+				webix.ajax().post("https://api.redaktr.com/tree", webix.ajax().stringify($$("tree").data.serialize()), function(text, xml, xhr) {
 					webix.message("Tree save complete");
 				});
 			});
 		};
 		var onSelectFnc = function(id) {
-			lastXHRGet.abort();
-			webix.ajax().get("https://api.redaktr.com/page/" + id, null, function(text, xml, xhr) {
-					webix.message(text);
+			if (lastXHRGet) { lastXHRGet.abort(); }
+			webix.ajax().get("https://api.redaktr.com/content/" + id, null, function(text, xml, xhr) {
+				$$("tinymce").setValue(text);
 			});
 		};
 		return {
@@ -35,9 +36,15 @@ export default class ContentView extends JetView {
 							animate: false,
 							keepViews: true,
 							cells: [{
-								id: "Visual",
+								id: "tinymce",
 								view: "tinymce-editor",
-								value: '<h2>Text editor</h2> <div><small>From Wikipedia, the free encyclopedia</small></div> <p>A <strong>text editor</strong> is a type of program used for editing plain text files </p>'
+								config: {
+									theme: "modern",
+									menubar: "edit insert view format table tools",
+									content_style: ".mce-content-body{font-size:14px;padding:8px;}",
+									content_css: "//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css," +
+						"//fonts.googleapis.com/css?family=Alice|Andika|Anonymous+Pro|Arimo|Arsenal|Bad+Script|Comfortaa|Cormorant|Cormorant+Garamond|Cormorant+Infant|Cormorant+SC|Cormorant+Unicase|Cousine|Cuprum|Didact+Gothic|EB+Garamond|El+Messiri|Exo+2|Fira+Mono|Fira+Sans|Fira+Sans+Condensed|Fira+Sans+Extra+Condensed|Forum|Gabriela|Istok+Web|Jura|Kelly+Slab|Kurale|Ledger|Lobster|Lora|Marck+Script|Marmelad|Merriweather|Neucha|Noto+Sans|Noto+Serif|Old+Standard+TT|Open+Sans|Open+Sans+Condensed:300|Oranienbaum|Oswald|PT+Mono|PT+Sans|PT+Sans+Caption|PT+Sans+Narrow|PT+Serif|PT+Serif+Caption|Pangolin|Pattaya|Philosopher|Play|Playfair+Display|Playfair+Display+SC|Podkova|Poiret+One|Prata|Press+Start+2P|Prosto+One|Roboto|Roboto+Condensed|Roboto+Mono|Roboto+Slab|Rubik|Rubik+Mono+One|Ruslan+Display|Russo+One|Scada|Seymour+One|Source+Sans+Pro|Stalinist+One|Tenor+Sans|Tinos|Ubuntu|Ubuntu+Condensed|Ubuntu+Mono|Underdog|Yanone+Kaffeesatz|Yeseva+One&amp;subset=cyrillic"
+								}
 							}, {
 								id: "Source",
 								view: "ace-editor",
@@ -140,7 +147,7 @@ export default class ContentView extends JetView {
 							editValue: "value",
 							editaction: "dblclick",
 							//drag:"order",
-							url: "https://api.redaktr.com/index",
+							url: "https://api.redaktr.com/tree",
 							on: {
 								"data->onAfterAdd": onChangeFnc,
 								"data->onAfterDelete": onChangeFnc,
