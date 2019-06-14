657f411a4f7d0a51ef71098cce3fc81bf5d14833
jbruwes <jbruwes@icloud.com>
2019-06-14 05:58:27 -0400

    chpock

--- sources/views/top.js
+++ sources/views/top.js
@@ -1,70 +1,85 @@
-import { JetView } from "webix-jet";
+import {
+  JetView
+} from "webix-jet";
 export default class TopView extends JetView {
-	config() {
-		var ui = {
-			rows: [{
-					view: "toolbar",
-					height: 56,
-					//css: "webix_dark",
-					elements: [{
-							view: "icon",
-							icon: "mdi mdi-menu",
-							click: () => {
-								$$("sidebar").toggle();
-							}
-						},
-						{ view: "label", label: "<span class='mdi mdi-36px mdi-glassdoor logoRedaktr'></span> <span class='mdi mdi-dark mdi-24px mdi-alpha-r'></span><span class='mdi mdi-dark mdi-24px mdi-alpha-e'></span><span class='mdi mdi-dark mdi-24px mdi-alpha-d'></span><span class='mdi mdi-dark mdi-24px mdi-alpha-a'></span><span class='mdi mdi-dark mdi-24px mdi-alpha-k'></span><span class='mdi mdi-dark mdi-24px mdi-alpha-t'></span><span class='mdi mdi-dark mdi-24px mdi-alpha-r'></span>" },
-						{
-							view: "icon",
-							icon: "mdi mdi-help-circle-outline",
-							click: () => {
-								window.open("https://redaktr.com/spravka/", "_blank");
-							}
-						}
-					]
-				},
-				{
-					cols: [{
-							view: "sidebar",
-							collapsed: true,
-							id: "sidebar",
-							//css: "webix_dark",
-							data: [],
-							click: (id) => {
-								if (id === "signout") webix.delay(_ => {
-									delete AWS.config.credentials.params.Logins['accounts.google.com'];
-									//delete AWS.config.credentials.params.Logins['graph.facebook.com'];
-									this.show('signin');
-									this._resetSidebar();
-								});
-								else this.show(id);
-							}
-						},
-						{
-							type: "wide",
-							padding: 2,
-							css: "app_layout",
-							rows: [
-								{ $subview: true }
-							]
-						}
-					]
-				}
+  config() {
+    var ui = {
+      rows: [{
+          view: "toolbar",
+          height: 56,
+          //css: "webix_dark",
+          elements: [{
+              view: "icon",
+              icon: "mdi mdi-menu",
+              click: () => {
+                $$("sidebar").toggle();
+              }
+            },
+            {
+              view: "label",
+              label: "<span class='mdi mdi-36px mdi-glassdoor logoRedaktr'></span> <span class='mdi mdi-dark mdi-24px mdi-alpha-r'></span><span class='mdi mdi-dark mdi-24px mdi-alpha-e'></span><span class='mdi mdi-dark mdi-24px mdi-alpha-d'></span><span class='mdi mdi-dark mdi-24px mdi-alpha-a'></span><span class='mdi mdi-dark mdi-24px mdi-alpha-k'></span><span class='mdi mdi-dark mdi-24px mdi-alpha-t'></span><span class='mdi mdi-dark mdi-24px mdi-alpha-r'></span>"
+            },
+            {
+              view: "icon",
+              icon: "mdi mdi-help-circle-outline",
+              click: () => {
+                window.open("https://redaktr.com/spravka/", "_blank");
+              }
+            }
+          ]
+        },
+        {
+          cols: [{
+              view: "sidebar",
+              collapsed: true,
+              id: "sidebar",
+              //css: "webix_dark",
+              data: [],
+              click: (id) => {
+                if (id === "signout") webix.delay(_ => {
+                  delete AWS.config.credentials.params.Logins['accounts.google.com'];
+                  //delete AWS.config.credentials.params.Logins['graph.facebook.com'];
+                  this.show('signin');
+                  this._resetSidebar();
+                });
+                else this.show(id);
+              }
+            },
+            {
+              type: "wide",
+              padding: 2,
+              css: "app_layout",
+              rows: [{
+                $subview: true
+              }]
+            }
+          ]
+        }
 
-			]
-		};
-		return ui;
-	}
-	_resetSidebar() {
-		$$("sidebar").clearAll();
-		$$("sidebar").add({ id: "signin", icon: "mdi mdi-login-variant", value: "Sign In" });
-		$$("sidebar").add({ id: "about", icon: "mdi mdi-information-outline", value: "About" });
-		$$("sidebar").select("signin");
-	}
-	init() {
-		$$("sidebar").getPopup().attachEvent("onBeforeShow", () => { return false; });
-		this._resetSidebar();
-	}
+      ]
+    };
+    return ui;
+  }
+  _resetSidebar() {
+    $$("sidebar").clearAll();
+    $$("sidebar").add({
+      id: "signin",
+      icon: "mdi mdi-login-variant",
+      value: "Sign In"
+    });
+    $$("sidebar").add({
+      id: "about",
+      icon: "mdi mdi-information-outline",
+      value: "About"
+    });
+    $$("sidebar").select("signin");
+  }
+  init() {
+    $$("sidebar").getPopup().attachEvent("onBeforeShow", () => {
+      return false;
+    });
+    this._resetSidebar();
+  }
 }
 /* global webix */
 /* global AWS */
