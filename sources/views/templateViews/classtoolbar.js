5ec7778e9d4b8a3e91a8f607cb2589957f7a08ca
jbruwes <jbruwes@icloud.com>
2019-07-15 06:09:20 -0400

    chpock

--- sources/views/templateViews/classtoolbar.js
+++ sources/views/templateViews/classtoolbar.js
@@ -1,26 +1,33 @@
-import { JetView } from "webix-jet";
+import {
+	JetView
+} from "webix-jet";
 export default class ClassToolbarView extends JetView {
-    config() {
-        return {
-            view: "toolbar",
-            cols: [{
-                view: "icon",
-                icon: "mdi mdi-file-document-outline",
-                click: _ => { $$("class").select($$('class').add({ class: '' })) }
-            }, {
-                view: "icon",
-                icon: "mdi mdi-delete-outline",
-                click: _ => {
-                    var id = $$("class").getSelectedId();
-                    if (id) {
-                        var newId = $$("class").getPrevId(id);
-                        if (!newId) newId = $$("class").getNextId(id);
-                        $$("class").remove(id);
-                        if (newId) $$("class").select(newId);
-                    }
-                }
-            }, {}]
-        };
-    }
+	config() {
+		return {
+			view: "toolbar",
+			cols: [{
+				view: "icon",
+				icon: "mdi mdi-file-document-outline",
+				click: _ => {
+					$$("class").select($$('class').add({
+						class: ''
+					}))
+				}
+			}, {
+				view: "icon",
+				icon: "mdi mdi-delete-outline",
+				click: _ => {
+					var id = $$("class").getSelectedId();
+					if (id) {
+						$$("class").editCancel();
+						var newId = $$("class").getPrevId(id);
+						if (!newId) newId = $$("class").getNextId(id);
+						$$("class").remove(id);
+						if (newId) $$("class").select(newId);
+					}
+				}
+			}, {}]
+		};
+	}
 }
 /* global $$ */
--- sources/views/templateViews/datatoolbar.js
+++ sources/views/templateViews/datatoolbar.js
@@ -1,26 +1,34 @@
-import { JetView } from "webix-jet";
+import {
+	JetView
+} from "webix-jet";
 export default class DataToolbarView extends JetView {
-    config() {
-        return {
-            view: "toolbar",
-            cols: [{
-                view: "icon",
-                icon: "mdi mdi-file-document-outline",
-                click: _ => { $$("data").select($$('data').add({ data: '', value: '' })) }
-            }, {
-                view: "icon",
-                icon: "mdi mdi-delete-outline",
-                click: _ => {
-                    var id = $$("data").getSelectedId();
-                    if (id) {
-                        var newId = $$("data").getPrevId(id);
-                        if (!newId) newId = $$("data").getNextId(id);
-                        $$("data").remove(id);
-                        if (newId) $$("data").select(newId);
-                    }
-                }
-            }, {}]
-        };
-    }
+	config() {
+		return {
+			view: "toolbar",
+			cols: [{
+				view: "icon",
+				icon: "mdi mdi-file-document-outline",
+				click: _ => {
+					$$("data").select($$('data').add({
+						data: '',
+						value: ''
+					}))
+				}
+			}, {
+				view: "icon",
+				icon: "mdi mdi-delete-outline",
+				click: _ => {
+					var id = $$("data").getSelectedId();
+					if (id) {
+						$$("data").editCancel();
+						var newId = $$("data").getPrevId(id);
+						if (!newId) newId = $$("data").getNextId(id);
+						$$("data").remove(id);
+						if (newId) $$("data").select(newId);
+					}
+				}
+			}, {}]
+		};
+	}
 }
 /* global $$ */
