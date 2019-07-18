17b335ee149c8df33d67032ccb10a15cfa07c8f0
jbruwes <jbruwes@icloud.com>
2019-07-18 06:02:46 -0400

    chpock

--- sources/views/template.js
+++ sources/views/template.js
@@ -426,7 +426,6 @@ export default class TemplateView extends JetView {
 			) +
 			'<script>' +
 			"document.write('<link rel=\"stylesheet\" href=\"//base.redaktr.com/" + AWS.config.credentials.identityId + ".cdn.css'+(window.location.hostname===\"www.redaktr.com\"||window.location.hostname===\"redaktr-jbruwes.codeanyapp.com\"?\"?\"+window.btoa(Math.random()):window.location.search.charAt(0)+window.btoa(unescape(encodeURIComponent(window.location.search))))+'\">');" +
-			"document.write('<link rel=\"stylesheet\" href=\"//base.redaktr.com/" + AWS.config.credentials.identityId + ".cdn.css'+(window.location.hostname===\"www.redaktr.com\"||window.location.hostname===\"redaktr-jbruwes.codeanyapp.com\"?\"?\"+window.btoa(Math.random()):window.location.search.charAt(0)+window.btoa(unescape(encodeURIComponent(window.location.search))))+'\">');" +
 			"document.write('<link rel=\"stylesheet\" href=\"//base.redaktr.com/" + AWS.config.credentials.identityId + ".css'+(window.location.hostname===\"www.redaktr.com\"||window.location.hostname===\"redaktr-jbruwes.codeanyapp.com\"?\"?\"+window.btoa(Math.random()):window.location.search.charAt(0)+window.btoa(unescape(encodeURIComponent(window.location.search))))+'\">');" +
 			'</script>' +
 			'<script src="//cdn.redaktr.com/require.min.js" defer></script>' +
