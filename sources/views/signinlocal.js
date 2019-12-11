17d08232df1ae86e426dc408511625ab73211371
jbruwes <jbruwes@icloud.com>
2019-12-11 03:35:08 -0500

    chpock

--- sources/views/signinlocal.js
+++ sources/views/signinlocal.js
@@ -1,189 +1,212 @@
 import {
-    JetView
+  JetView
 } from "webix-jet";
 export default class SignInViewLocal extends JetView {
-    config() {
-        var that,
-            /*AmazonCognitoIdentity = require('amazon-cognito-identity-js'),
+  config() {
+    var that,
+      /*AmazonCognitoIdentity = require('amazon-cognito-identity-js'),
 			userPool = this.app.userPool = new AmazonCognitoIdentity.CognitoUserPool({
 				UserPoolId: 'us-east-1_isPFINeJO',
 				ClientId: '4vvur02v4d5smj3pvtj0tu8qda'
 			}),*/
-            appShow = item => {
-                $$("sidebar").clearAll();
-                //$$("toolbar").addView({
-                //	id: "play",
-                //	view: "icon",
-                //	icon: "mdi mdi-play-circle",
-                //	click: _ => {
-                //		window.open("https://redaktr.com/" + item.name + "/?" + webix.uid(), "_tab");
-                //	}
-                //});
-                this.show("content").then(function(value) {
-                    $$("tinymce").getEditor(true).then(editor => {
-                        $$("sidebar").add({
-                            id: "content",
-                            icon: "mdi mdi-book-open-page-variant",
-                            value: "Content"
-                        }, 0);
-                        $$("sidebar").add({
-                            id: "template",
-                            icon: "mdi mdi-language-html5",
-                            value: "Template"
-                        }, 1);
-                        $$("sidebar").add({
-                            id: "css",
-                            icon: "mdi mdi-language-css3",
-                            value: "CSS"
-                        }, 2);
-                        $$("sidebar").add({
-                            id: "js",
-                            icon: "mdi mdi-language-javascript",
-                            value: "JavaScript"
-                        }, 3);
-                        //$$("sidebar").add({
-                        //    id: "settings",
-                        //    icon: "mdi mdi-settings",
-                        //    value: "Settings"
-                        //}, 4);
-                        $$("sidebar").add({
-                            id: "signout",
-                            icon: "mdi mdi-logout-variant",
-                            value: "Sign Out"
-                        });
-                        $$("sidebar").select("content");
-                    });
-                }, function(reason) {
-                    webix.message({
-                        text: "Something goes wrong",
-                        type: "error"
-                    });
-                });
-            },
-            clickLogin = _ => {
-                if (!this.authenticationData || !(this.authenticationData.Username === $$('username').getValue() && this.authenticationData.Password === $$('password').getValue())) {
-                    this.authenticationData = {
-                        Username: $$('username').getValue(),
-                        Password: $$('password').getValue(),
-                    }
-                  
-                  
-                  webix.ajax().post("//s3.redaktr:9000/minio/webrpc",
-                  {"id":1,"jsonrpc":"2.0","params":{"username":$$("username").getValue(),"password":$$("password").getValue()},"method":"Web.Login"}
-                                   ).then(function(res){
-    // response
-                    console.log(res);
-                    console.log(res.json());
-});
-                  
-                    $.ajax({
-                        url: "http://s3.redaktr:9000/minio/webrpc",
-                        type: "POST",
-                        dataType: "json",
-                        contentType: "application/json",
-                        data: '{"id":1,"jsonrpc":"2.0","params":{"username":"' + $$("username").getValue() + '","password":"' + $$("password").getValue() + '"},"method":"Web.Login"}',
-                        success: (res) => {
-                            if (res.error) webix.message({
-                                text: res.error.message,
-                                type: "error"
-                            });
-                            else {
-                                this.app.S3 = new AWS.S3({
-                                    correctClockSkew: true,
-                                    useAccelerateEndpoint: true,
-                                    accessKeyId: $$('username').getValue(),
-                                    secretAccessKey: $$('password').getValue(),
-                                    endpoint: 'http://s3.redaktr',
-                                    s3ForcePathStyle: true,
-                                    signatureVersion: 'v4'
-                                });
-                                this.app.identityId = 'us-east-1:' + $$('username').getValue();
-                                appShow($$('username').getValue());
-                            }
-                        }
+      appShow = item => {
+        $$("sidebar").clearAll();
+        //$$("toolbar").addView({
+        //	id: "play",
+        //	view: "icon",
+        //	icon: "mdi mdi-play-circle",
+        //	click: _ => {
+        //		window.open("https://redaktr.com/" + item.name + "/?" + webix.uid(), "_tab");
+        //	}
+        //});
+        this.show("content").then(function(value) {
+          $$("tinymce").getEditor(true).then(editor => {
+            $$("sidebar").add({
+              id: "content",
+              icon: "mdi mdi-book-open-page-variant",
+              value: "Content"
+            }, 0);
+            $$("sidebar").add({
+              id: "template",
+              icon: "mdi mdi-language-html5",
+              value: "Template"
+            }, 1);
+            $$("sidebar").add({
+              id: "css",
+              icon: "mdi mdi-language-css3",
+              value: "CSS"
+            }, 2);
+            $$("sidebar").add({
+              id: "js",
+              icon: "mdi mdi-language-javascript",
+              value: "JavaScript"
+            }, 3);
+            //$$("sidebar").add({
+            //    id: "settings",
+            //    icon: "mdi mdi-settings",
+            //    value: "Settings"
+            //}, 4);
+            $$("sidebar").add({
+              id: "signout",
+              icon: "mdi mdi-logout-variant",
+              value: "Sign Out"
+            });
+            $$("sidebar").select("content");
+          });
+        }, function(reason) {
+          webix.message({
+            text: "Something goes wrong",
+            type: "error"
+          });
+        });
+      },
+      clickLogin = _ => {
+        if (!this.authenticationData || !(this.authenticationData.Username === $$('username').getValue() && this.authenticationData.Password === $$('password').getValue())) {
+          this.authenticationData = {
+            Username: $$('username').getValue(),
+            Password: $$('password').getValue(),
+          }
 
-                    });
-                  
-                  
-                  
-                }
+
+          webix.ajax().headers({
+            "Content-type": "application/json"
+          }).post("//s3.redaktr:9000/minio/webrpc", {
+            "id": 1,
+            "jsonrpc": "2.0",
+            "params": {
+              "username": $$("username").getValue(),
+              "password": $$("password").getValue()
             },
-            result = {
-                css: "signInView",
-                cols: [{
-                    gravity: 0.38
-                }, {
-                    css: "signInViewRight",
-                    rows: [{
-                        css: "signInViewField",
-                        padding: 0,
-                        cols: [{
-                            rows: [{
-                                view: "form",
-                                id: "log_form",
-                                width: 272,
-                                borderless: true,
-                                elements: [{
-                                        view: "text",
-                                        placeholder: "Username",
-                                        name: "username",
-                                        id: "username"
-                                    },
-                                    {
-                                        view: "text",
-                                        type: "password",
-                                        placeholder: "Password",
-                                        name: "password",
-                                        id: "password"
-                                    },
-                                    {
-                                        view: "button",
-                                        value: "Login",
-                                        css: "webix_primary",
-                                        click: clickLogin
-                                    }
-                                ]
-                            }]
-                        }, {}]
-                    }, {}, {
-                        padding: 2,
-                        cols: [{}, {
-                            id: "www",
-                            view: "button",
-                            css: "webix_transparent",
-                            type: "icon",
-                            width: 268,
-                            label: "𝔚𝔢𝔫𝔦𝔤 𝔚𝔢𝔟 𝔚𝔬𝔯𝔨𝔰𝔥𝔬𝔭",
-                            icon: "mdi mdi-cursor-default-click-outline",
-                            click: () => {
-                                window.open("https://w--w--w.com", "_blank");
-                            }
-                        }]
-                    }]
-                }]
-            };
-        if (this.app.config.size === "wide") result.cols[1].rows.unshift({
-            //id: "header_template",
-            view: "template",
-            template: "<h1 class='redaktrHeader'>REDAKTR<div>website control&nbsp;panel</div></h1>",
-            minHeight: 150,
-            type: "clean"
-        });
-        result.cols[1].rows.unshift({
-            gravity: (this.app.config.size === "wide") ? 2 : 1
-        });
-        if (this.app.config.size === "wide") result.cols[1].rows.unshift({
-            paddingX: 10,
+            "method": "Web.Login"
+          }).then(function(data) {
+            var res = data.json();
+            if (res.error) webix.message({
+              text: res.error.message,
+              type: "error"
+            });
+            else {
+              this.app.S3 = new AWS.S3({
+                correctClockSkew: true,
+                useAccelerateEndpoint: true,
+                accessKeyId: $$('username').getValue(),
+                secretAccessKey: $$('password').getValue(),
+                endpoint: 'http://s3.redaktr',
+                s3ForcePathStyle: true,
+                signatureVersion: 'v4'
+              });
+              this.app.identityId = 'us-east-1:' + $$('username').getValue();
+              appShow($$('username').getValue());
+            }
+          });
+          /*                  
+                              $.ajax({
+                                  url: "http://s3.redaktr:9000/minio/webrpc",
+                                  type: "POST",
+                                  dataType: "json",
+                                  contentType: "application/json",
+                                  data: '{"id":1,"jsonrpc":"2.0","params":{"username":"' + $$("username").getValue() + '","password":"' + $$("password").getValue() + '"},"method":"Web.Login"}',
+                                  success: (res) => {
+                                      if (res.error) webix.message({
+                                          text: res.error.message,
+                                          type: "error"
+                                      });
+                                      else {
+                                          this.app.S3 = new AWS.S3({
+                                              correctClockSkew: true,
+                                              useAccelerateEndpoint: true,
+                                              accessKeyId: $$('username').getValue(),
+                                              secretAccessKey: $$('password').getValue(),
+                                              endpoint: 'http://s3.redaktr',
+                                              s3ForcePathStyle: true,
+                                              signatureVersion: 'v4'
+                                          });
+                                          this.app.identityId = 'us-east-1:' + $$('username').getValue();
+                                          appShow($$('username').getValue());
+                                      }
+                                  }
+
+                              });
+          */
+
+
+        }
+      },
+      result = {
+        css: "signInView",
+        cols: [{
+          gravity: 0.38
+        }, {
+          css: "signInViewRight",
+          rows: [{
+            css: "signInViewField",
+            padding: 0,
+            cols: [{
+              rows: [{
+                view: "form",
+                id: "log_form",
+                width: 272,
+                borderless: true,
+                elements: [{
+                    view: "text",
+                    placeholder: "Username",
+                    name: "username",
+                    id: "username"
+                  },
+                  {
+                    view: "text",
+                    type: "password",
+                    placeholder: "Password",
+                    name: "password",
+                    id: "password"
+                  },
+                  {
+                    view: "button",
+                    value: "Login",
+                    css: "webix_primary",
+                    click: clickLogin
+                  }
+                ]
+              }]
+            }, {}]
+          }, {}, {
+            padding: 2,
             cols: [{}, {
-                view: "label",
-                label: '<div class="redaktr-circle-logo-container"><div class="redaktr-circle-logo"><span class="mdi mdi-48px mdi-glassdoor largeLogoRedaktr"></span></div></div>',
-                height: 152,
-                width: 152
+              id: "www",
+              view: "button",
+              css: "webix_transparent",
+              type: "icon",
+              width: 268,
+              label: "𝔚𝔢𝔫𝔦𝔤 𝔚𝔢𝔟 𝔚𝔬𝔯𝔨𝔰𝔥𝔬𝔭",
+              icon: "mdi mdi-cursor-default-click-outline",
+              click: () => {
+                window.open("https://w--w--w.com", "_blank");
+              }
             }]
-        });
-        webix.UIManager.addHotKey("enter", clickLogin);
-        return result;
-    }
+          }]
+        }]
+      };
+    if (this.app.config.size === "wide") result.cols[1].rows.unshift({
+      //id: "header_template",
+      view: "template",
+      template: "<h1 class='redaktrHeader'>REDAKTR<div>website control&nbsp;panel</div></h1>",
+      minHeight: 150,
+      type: "clean"
+    });
+    result.cols[1].rows.unshift({
+      gravity: (this.app.config.size === "wide") ? 2 : 1
+    });
+    if (this.app.config.size === "wide") result.cols[1].rows.unshift({
+      paddingX: 10,
+      cols: [{}, {
+        view: "label",
+        label: '<div class="redaktr-circle-logo-container"><div class="redaktr-circle-logo"><span class="mdi mdi-48px mdi-glassdoor largeLogoRedaktr"></span></div></div>',
+        height: 152,
+        width: 152
+      }]
+    });
+    webix.UIManager.addHotKey("enter", clickLogin);
+    return result;
+  }
 }
 /* global AWS */
 /* global webix */
