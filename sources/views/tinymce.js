7126c2c456d1cb5719016360a96cb4773e3e14fc
jbruwes <jbruwes@icloud.com>
2020-08-21 07:37:26 -0400

    chpock

--- sources/views/tinymce.js
+++ sources/views/tinymce.js
@@ -43,8 +43,8 @@ export default class TinymceView extends JetView {
           },
           {
             title: 'carousel',
-            description: 'data-carousel data-pager data-auto data-path',
-            content: '<div class="mceNonEditable" data-carousel="" data-pager="false" data-auto="" data-path=""><div class="ui basic segment" style="height: 100vh; background-size: cover;"><div class="ui active very light dimmer"><a class="ui massive inverted icon header" href="#"><span class="sub header"><!-- --></span></a></div></div></div>'
+            description: 'data-pager data-auto data-path',
+            content: '<div class="mceNonEditable" data-id="carousel" data-pager="false" data-auto="" data-path=""><div class="ui basic segment" style="height: 100vh; background-size: cover;"><div class="ui active very light dimmer"><a class="ui massive inverted icon header" href="#"><span class="sub header"><!-- --></span></a></div></div></div>'
           },
           {
             title: 'particles',
