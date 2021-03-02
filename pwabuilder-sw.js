607771343236f5bdcb0eb9d997fbdf4e90f611ac
jbruwes <jbruwes@icloud.com>
2021-03-02 08:34:06 +0000

    workbox version update

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -3,7 +3,7 @@
 const CACHE = "pwabuilder-offline";
 const QUEUE_NAME = "bgSyncQueue";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/6.1.1/workbox-sw.min.js');
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
