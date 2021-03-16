d9111fdeaaa998dac889a4dc501dade7666e7195
Jerry Bruwes <jbruwes@icloud.com>
2021-03-16 15:16:32 +0300

    bgSyncPlugin remove

--- resource/pwabuilder-sw.js
+++ resource/pwabuilder-sw.js
@@ -1,7 +1,6 @@
 // This is the "Offline copy of assets" service worker
 
 const CACHE = "pwabuilder-offline";
-const QUEUE_NAME = "bgSyncQueue";
 
 importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/6.1.1/workbox-sw.min.js');
 
@@ -11,16 +10,9 @@ self.addEventListener("message", (event) => {
   }
 });
 
-const bgSyncPlugin = new workbox.backgroundSync.BackgroundSyncPlugin(QUEUE_NAME, {
-  maxRetentionTime: 24 * 60 // Retry for max of 24 Hours (specified in minutes)
-});
-
 workbox.routing.registerRoute(
   new RegExp('/*'),
   new workbox.strategies.NetworkFirst({
-    cacheName: CACHE,
-    plugins: [
-      bgSyncPlugin
-    ]
+    cacheName: CACHE
   })
 );
