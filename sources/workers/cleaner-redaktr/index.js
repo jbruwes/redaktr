aa6079086546f11c5a2f8740cb7aad6bd139f934
Jerry Bruwes <jbruwes@icloud.com>
2021-05-20 17:09:52 +0300

    workers

--- sources/workers/cleaner-redaktr/index.js
+++ sources/workers/cleaner-redaktr/index.js
@@ -1,43 +1,43 @@
-/*jshint esversion: 9 */
-const aws = require('aws-sdk'),
-	s3 = new aws.S3(),
-	documentClient = new aws.DynamoDB.DocumentClient();
-exports.handler = async (event, context) => {
-	async function* listAllKeys(opts) {
-		do {
-			const data = await s3.listObjectsV2(opts).promise();
-			opts.ContinuationToken = data.NextContinuationToken;
-			yield data;
-		} while (opts.ContinuationToken)
-	}
-	try {
-		let redaktr = await documentClient.scan({
-			TableName: "redaktr",
-			ProjectionExpression: "id"
-		}).promise();
-		redaktr = redaktr.Items.map(obj => {
-			return obj.id
-		});
-		for await (const data of listAllKeys({
-			Bucket: 'redaktr'
-		})) {
-			await Promise.all(data.Contents.map(async element => {
-				if (redaktr.indexOf(element.Key.split('.')[0]) === -1 && redaktr.indexOf(element.Key.split('/')[0]) === -1) {
-					console.log(element.Key);
-					const copy = await s3.copyObject({
-						Bucket: "smetnik",
-						CopySource: "/redaktr/" + encodeURIComponent(element.Key),
-						Key: element.Key
-					}).promise();
-					await s3.deleteObject({
-						Bucket: "redaktr",
-						Key: element.Key
-					}).promise(copy);
-				}
-			}));
-		}
-	} catch (err) {
-		console.log(err);
-		throw new Error('documentClient.scan');
-	}
+/* jshint esversion: 9 */
+const aws = require('aws-sdk');
+const s3 = new aws.S3();
+const documentClient = new aws.DynamoDB.DocumentClient();
+exports.handler = async(event, context) => {
+  async function * listAllKeys(opts) {
+    do {
+      const data = await s3.listObjectsV2(opts).promise();
+      opts.ContinuationToken = data.NextContinuationToken;
+      yield data;
+    } while (opts.ContinuationToken);
+  }
+  try {
+    let redaktr = await documentClient.scan({
+      TableName: 'redaktr',
+      ProjectionExpression: 'id',
+    }).promise();
+    redaktr = redaktr.Items.map((obj) => {
+      return obj.id;
+    });
+    for await (const data of listAllKeys({
+      Bucket: 'redaktr',
+    })) {
+      await Promise.all(data.Contents.map(async(element) => {
+        if (redaktr.indexOf(element.Key.split('.')[0]) === -1 && redaktr.indexOf(element.Key.split('/')[0]) === -1) {
+          console.log(element.Key);
+          const copy = await s3.copyObject({
+            Bucket: 'smetnik',
+            CopySource: '/redaktr/' + encodeURIComponent(element.Key),
+            Key: element.Key,
+          }).promise();
+          await s3.deleteObject({
+            Bucket: 'redaktr',
+            Key: element.Key,
+          }).promise(copy);
+        }
+      }));
+    }
+  } catch (err) {
+    console.log(err);
+    throw new Error('documentClient.scan');
+  }
 };
--- sources/workers/seo/index.js
+++ sources/workers/seo/index.js
@@ -1,38 +1,39 @@
-'use strict';
-let aws = require('aws-sdk');
-let s3 = new aws.S3();
-let documentClient = new aws.DynamoDB.DocumentClient();
-let html = require('./html');
+const aws = require('aws-sdk');
+const html = require('./html');
+const s3 = new aws.S3();
 exports.handler = (event, context, callback) => {
-	const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-	const id = key.match(/^[^\/]+/)[0];
-	Promise.all([documentClient.get({
-		TableName: "redaktr",
-		Key: {
-			"id": id
-		}
-	}).promise(), s3.getObject({
-		Bucket: 'redaktr',
-		Key: id + '/index.json',
-	}).promise(), s3.getObject({
-		Bucket: 'redaktr',
-		Key: id + '.html'
-	}).promise()]).then(value => html.main(value, id, key, s3, callback)).catch(err => {
-		console.log('index.js', id);
-		console.log('documentClient.get', {
-			TableName: "redaktr",
-			Key: {
-				"id": id
-			}
-		});
-		console.log('s3.getObject', {
-			Bucket: 'redaktr',
-			Key: id + '/index.json',
-		});
-		console.log('s3.getObject', {
-			Bucket: 'redaktr',
-			Key: id + '.html'
-		});
-		callback(err);
-	});
+  const key = decodeURIComponent(event.Records[0].s3.object.key
+    .replace(/\+/g, ' '));
+  const id = key.match(/^[^/]+/)[0];
+  Promise.all([s3.getObject({
+    Bucket: 'redaktr',
+    Key: id + '.json',
+  }).promise(), s3.getObject({
+    Bucket: 'redaktr',
+    Key: id + '/index.json',
+  }).promise(), s3.getObject({
+    Bucket: 'redaktr',
+    Key: id + '.html',
+  }).promise()]).then((value) => html.main(
+    value,
+    id,
+    key,
+    s3,
+    callback))
+    .catch((err) => {
+      console.log('index.js', id);
+      console.log('s3.getObject', {
+        Bucket: 'redaktr',
+        Key: id + '.json',
+      });
+      console.log('s3.getObject', {
+        Bucket: 'redaktr',
+        Key: id + '/index.json',
+      });
+      console.log('s3.getObject', {
+        Bucket: 'redaktr',
+        Key: id + '.html',
+      });
+      callback(err);
+    });
 };
